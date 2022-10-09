72cfb92688be720c1416a8d22561fcc5481e7094
Raymon <raymonclm@gmail.com>
2022-10-09 16:53:44 +0800

    first commit

---
+++ src/main/java/com/raymon/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.raymon.git;
+
+/**
+ * @author raymon
+ * @create 2022-10-09 16:49
+ */
+
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
